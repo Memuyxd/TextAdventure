7b8e0162ff89753fd4b77227db22179eefe29353
MarkKapileo <mark.kapileo@my.marianas.edu>
2024-03-27 17:51:15 +1000

    character class made

---
+++ src/Character.java
@@ -0,0 +1,17 @@
+public abstract class Character {
+    public String name;
+    public int maxHp, hp;
+
+    public Character(String name, int maxHp, int hp) {
+        this.name = name;
+        this.maxHp = maxHp;
+        this.hp = maxHp;
+    }
+
+    public Character() {
+
+    }
+
+    public abstract int attack();
+    public abstract int defend();
+}
