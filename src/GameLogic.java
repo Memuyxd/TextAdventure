40201945cfbfb203ab9475ac997fa3a9a84c2a2d
MarkKapileo <mark.kapileo@my.marianas.edu>
2024-03-27 17:51:28 +1000

    updated logic

--- src/GameLogic.java
+++ src/GameLogic.java
@@ -1,16 +1,32 @@
 import java.util.Scanner;
 
 public class GameLogic {
-public static int act = 1;
+public static int act = 1, place = 0;
+    double totalHealth;
+
+    int attackStat;
+    int defenseStat;
+    public static String[] encounters = {"Battle", "Battle", "Rest", "Rest"};
+    public static String[] monsters = {"Mushroom", "Goblin", "Thief", "Slime", "Mushroom" };
         public static int encountersEngaged = 0;
     static Scanner scanner = new Scanner(System.in);
+public static int readInt(String prompt, int userChoices){
+    int input;
+    do {
+        System.out.println(prompt);
+        try{
+            input = Integer.parseInt(scanner.next());
+        }catch(Exception e){
+            input =-1;
+            System.out.println("Please enter an integer!");
+        }
+    }while(input < 1 || input > userChoices);
+    return input;
+    }
 
     public class PlayerHealth {
 
-        double totalHealth;
 
-        int attackStat;
-        int defenseStat;
 
         public static final int EMOTIONAL_DAMAGE = 1;
         public static final int RUSSIAN_HAND_SLAP = 1;
@@ -51,14 +67,49 @@ public static int act = 1;
     public static void checkAct(){
 
         if(act == 1 && encountersEngaged == 1 ) {
-
+        place = 1;
         act = 2;
-            Lore.FirstAct();}
+            Lore.FirstAct();
+            //new monster
+        monsters[0] = "Mushroom";
+        monsters[1] = "Goblin";
+        monsters[2] = "Kangaroo";
+        monsters[3] = "Ogre";
+        monsters[4] = "Thief";
+        //new encounter value
+        encounters[0] = "Battle";
+        encounters[1] = "Battle";
+        encounters[2] = "Rest";
+        encounters[3] = "Battle";
+        encounters[4] = "Battle";
+        }
+        else if (act == 1 && encountersEngaged == 0) {
+
+
+        }
+    }
+    //RANDOM ENCOUNTER CONTROL
+    public static void randomEncounter(){
+        //random number from 0 to array length
+        int encounter = (int) (Math.random()* encounters.length);
+        if(encounters[encounter].equals("Battle"));
+            //randomBattle();
+           // else(encounters[encounter].equals("Rest"))
+                //takeRest();
+    }
+
+    public static void journeyForth(){
+        //check if act must be increased
+        checkAct();
+        //check if final act
+        if(act != 3){
+            randomEncounter();
+        }
     }
     public static void characterInfo() {
             clearConsole();
         System.out.println("======Character Info======");
-        System.out.println("HP:" + currentHealth + "/" + totalHealth);
+        System.out.println("HP:100");
         printSeperator(20);
         System.out.println();
 
